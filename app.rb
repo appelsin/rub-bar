caac43a5b0dfea3d08ccb517a3c596de51fa50f0
appelsin <rexe@ya.ru>
2014-12-14 16:06:50 +0300

    remove extra cache + 6 month period

--- app.rb
+++ app.rb
@@ -4,15 +4,12 @@ require_relative 'lib/stupid_cache'
 require_relative 'lib/chart_model'
 
 get '/' do
-  slim :chart #, layout: :app
+  slim :chart
 end
 
 get '/chart/rub_bar.json' do
   result_json = StupidCache.fetch :rub_bar do
-    result_data = StupidCache.fetch :rub_bar do
-      ChartModel.get_rub_bar
-    end
-    Oj.dump(result_data, mode: :compat)
+    Oj.dump ChartModel.get_rub_bar, mode: :compat
   end
 
   content_type 'application/json'
